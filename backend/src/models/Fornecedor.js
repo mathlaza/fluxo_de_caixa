93946af6934c504a6279ced7352184c6dd5295c3
Matheus Martinez <decretomusic@gmail.com>
2023-06-28 18:29:43 -0300

    feat: Fornecedor table model

---
+++ backend/src/models/Fornecedor.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+  const LojaTable = sequelize.define('Dim_Fornecedores', {
+    ID_fornecedor: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Nome: {
+      type: DataTypes.STRING
+    },
+    Sobrenome: {
+      type: DataTypes.STRING
+    },
+    Estado: {
+      type: DataTypes.STRING
+    },
+    Cidade: {
+      type: DataTypes.STRING
+    },
+    Endereço: {
+      type: DataTypes.STRING
+    },
+    Numero: {
+      type: DataTypes.INTEGER
+    },
+    Telefone: {
+      type: DataTypes.STRING
+    },
+    CPF: {
+      type: DataTypes.STRING
+    },
+    CNPJ: {
+      type: DataTypes.STRING
+    },
+    CEP: {
+      type: DataTypes.STRING
+    }
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'Dim_Fornecedores'
+    }
+  );
+
+  LojaTable.associate = (models) => {
+    LojaTable.hasMany(models.ProdutosTable,
+    { foreignKey: 'ID_fornecedor', as: 'fornecedor' });
+  };
+
+  return FornecedorTable;
+};
